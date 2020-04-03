769bf57e7d7a4cd762580424aa3f097e33b77ec9
lwzhenglittle <924695205@qq.com>
2020-04-03 14:24:29 +0800

    remake arch

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import config
 import flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_socketio import SocketIO
 app = flask.Flask("Lilm")
 app.config["SQLALCHEMY_DATABASE_URI"] = config.SQL_URL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+socket = SocketIO(app)
---
+++ route/__init__.py
@@ -0,0 +1,3 @@
+from .api_add import *
+from .api_delete import *
+from .api_query import *
